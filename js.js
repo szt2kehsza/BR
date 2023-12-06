828b4bef36e1fb5b9d7615ab82103f6dc47c1e0a
szt2kehsza <115459926+szt2kehsza@users.noreply.github.com>
2023-12-06 21:08:15 +0100

    elkezdve

---
+++ js.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.querySelector(".menuIcon");
+    const menu = document.querySelector(".menu");
+    const closeBtn = document.querySelector(".closeBtn");
+  
+    menuIcon.addEventListener("click", function() {
+      menu.style.display = "block";
+    });
+  
+    closeBtn.addEventListener("click", function() {
+      menu.style.display = "none";
+    });
+  });
+  
