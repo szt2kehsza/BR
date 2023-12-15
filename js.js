6c38e546490efaa02f0bd619f8f78c530c518ed3
szt2kehsza <115459926+szt2kehsza@users.noreply.github.com>
2023-12-15 10:21:54 +0100

    b

--- js.js
+++ js.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuIcon = document.querySelector(".menuIcon");
-    const menu = document.querySelector(".menu");
-    const closeBtn = document.querySelector(".closeBtn");
+// document.addEventListener("DOMContentLoaded", function() {
+//     const menuIcon = document.querySelector(".menuIcon");
+//     const menu = document.querySelector(".menu");
+//     const closeBtn = document.querySelector(".closeBtn");
   
-    menuIcon.addEventListener("click", function() {
-      menu.style.display = "block";
-    });
+//     menuIcon.addEventListener("click", function() {
+//       menu.style.display = "block";
+//     });
   
-    closeBtn.addEventListener("click", function() {
-      menu.style.display = "none";
-    });
-  });
+//     closeBtn.addEventListener("click", function() {
+//       menu.style.display = "none";
+//     });
+//   });
   
